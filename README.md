33f624fd2ef39571991ac9c69fe3bad1c7fa208d
devlooppear <115846304+devlooppear@users.noreply.github.com>
2023-04-05 20:46:39 -0300

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# MySQL_API
- Este é um código em Python que utiliza o pacote mysql-connector-python para automatizar operações com o MySQL, como criação de tabelas, inserção, atualização e exclusão de dados. O objetivo é tornar essas operações mais práticas e eficientes, como inserção de várias variáveis de uma só vez.
+# MySQL Python API
+
+- Este é um exemplo de como utilizar o MySQL em Python utilizando o pacote mysql-connector-python. O objetivo é automatizar e facilitar operações dentro de um ou mais banco de dados com o Sistema Gerenciador de Banco de Dados MySQL, como criar tabelas, inserir, atualizar e excluir dados de forma mais p´ratica, por exemplo, inserindo várias variáveis de forma mais prática.
+
+## Pré-requisitos
+
+- Para executar esse código é necessário ter instalado o Python e o pacote mysql-connector-python. Além disso, é preciso ter um servidor MySQL.
+
+## Como utilizar
+
+1 - Importe a biblioteca:
+```python
+import mysql.connector
+```
+2 - Crie uma conexão com o banco de dados. Adapte os dados de acesso em:
+```python
+HOST = 'localhost'
+USER = 'root'
+PASSWORD = '12345'
+DATABASE = ''
+```
+3 - Crie uma instância da classe MySQLDB:
+```python
+db = MySQLDB()
+```
+4 - Execute as operações desejadas:
+```python
+# Criar tabela
+db.create_table('clientes')
+
+# Inserir dados
+db.insert_data('clientes', '1, "João", "joao@gmail.com"')
+
+# Selecionar dados
+db.select_data('clientes', '*', 'id=1')
+
+# Atualizar dados
+db.update_data('clientes', 'email', 'joao@outlook.com', 'id=1')
+
+# Excluir dados
+db.delete_data('clientes', 'id=1')
+
+# Adicionar coluna
+db.add_column('clientes', 'idade', 'INT')
+
+# Excluir tabela
+db.drop_table('clientes')
+
+# Fechar conexão
+db.close_connection()
+```
+
+## Ferramentas
+
+- MySQL
+- Python
---
+++ main.py
@@ -0,0 +1,89 @@
+import mysql.connector
+
+HOST = 'localhost'
+USER = 'root'
+PASSWORD = '12345'
+DATABASE = ''
+
+class MySQLDB:
+    def __init__(self):
+        self.host = HOST
+        self.user = USER
+        self.password = PASSWORD
+        self.database = DATABASE
+        self.connection = mysql.connector.connect(
+            host=self.host,
+            user=self.user,
+            password=self.password,
+            database=self.database
+        )
+        self.cursor = self.connection.cursor()
+
+    def execute_query(self, query):
+        self.cursor.execute(query)
+        self.connection.commit()
+        return self.cursor.fetchall()
+
+    def create_table(self, table_name):
+        query = f'''
+        CREATE TABLE IF NOT EXISTS {table_name} (
+        id INT NOT NULL AUTO_INCREMENT,
+        PRIMARY KEY (id)
+        ) DEFAULT CHARSET = utf8mb4;
+        '''
+        return self.execute_query(query)
+
+    def drop_table(self, table_name):
+        query = f'''
+        DROP TABLE IF EXISTS {table_name};
+        '''
+        return self.execute_query(query)
+
+    def insert_data(self, table_name, values):
+        query = f'''
+        INSERT INTO {table_name} 
+        VALUES ({values});
+        '''
+        return self.execute_query(query)
+
+    def select_data(self, table_name, column='*', condition=None):
+        query = f'''
+        SELECT {column} FROM {table_name}
+        '''
+        if condition:
+            query += f' WHERE {condition}'
+        return self.execute_query(query)
+
+    def update_data(self, table_name, column, value, condition):
+        query = f'''
+        UPDATE {table_name}
+        SET {column} = '{value}'
+        WHERE {condition};
+        '''
+        return self.execute_query(query)
+
+    def add_column(self, table_name, column_name, data_type):
+        query = f'''
+        ALTER TABLE {table_name}
+        ADD COLUMN {column_name} {data_type} FIRST;
+        '''
+        return self.execute_query(query)
+
+    def delete_data(self, table_name, condition, limit=None):
+        query = f'''
+        DELETE FROM {table_name}
+        WHERE {condition}
+        '''
+        if limit:
+            query += f'LIMIT {limit}'
+        return self.execute_query(query)
+
+    def drop_database(self, database_name):
+        query = f'''
+        DROP DATABASE IF EXISTS {database_name};
+        '''
+        return self.execute_query(query)
+
+    def close_connection(self):
+        self.cursor.close()
+        self.connection.close()
